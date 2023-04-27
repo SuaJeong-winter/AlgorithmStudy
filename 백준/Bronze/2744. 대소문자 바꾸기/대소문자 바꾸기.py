00a05737437fbd0f1082c49700b4015bf4c7b749
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-04-28 03:58:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -2,7 +2,5 @@ txt=list(input())
 for i in range(len(txt)):
     if 65<=ord(txt[i])<=90:
         print(chr(ord(txt[i])+32), end='')
-    elif 97<=ord(txt[i])<=120:
+    if 97<=ord(txt[i])<=120:
         print(chr(ord(txt[i])-32), end='')
-    else:
-      pass
