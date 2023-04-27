6c3e988b74bac4a50540570e18e7d9154922ffeb
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-04-27 12:47:52 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+locx = int(input())
+locy = int(input())
+if (locx >0):
+    if (locy >0):
+        print("1")
+    else:
+        print("4")
+else:
+    if(locy >0):
+        print("2")
+    else:
+        print("3")
