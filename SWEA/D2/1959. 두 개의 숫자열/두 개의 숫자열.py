2dea648a3ad8225b2b64423b42a0e26814163bc7
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-05-17 17:17:42 +0900

    [D2] Title: 두 개의 숫자열, Time: 152 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for t in range(1, T+1) :
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    if N == M :
+        max = 0
+        for i in range(N) :
+            max += A[i] * B[i]
+    else :
+        if N > M :
+            sum = [0] * (N-M+1)
+            for i in range(len(sum)) :
+                for j in range(0, M, 1) :
+                    sum[i] += A[j+i] * B[j]
+        else :
+            sum = [0] * (M-N+1)
+            for i in range(len(sum)) :
+                for j in range(0, N, 1) :
+                    sum[i] += A[j] * B[j+i]
+        sum.sort()
+        max = sum[-1]
+
+    print("#{} {}".format(t, max))
