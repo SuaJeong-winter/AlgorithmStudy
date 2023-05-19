be0ac9b1d7bf5124881994132b659045f4ddc22d
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-05-19 12:46:28 +0900

    [D2] Title: 파스칼의 삼각형, Time: 99 ms, Memory: 45,848 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -3,21 +3,15 @@ T = int(input())
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
     n=int(input())
-    result = [[1] for _ in range(n)]
-    
-    for i in range(1,n):
-        if i==1:
-            result[i].append(1)
-            continue
-        for j in range(1,i):
-            result[i].append(result[i-1][j-1]+result[i-1][j])
-        result[i].append(1)
-        
-    for i in result:
-        if len(i)==1:
-            print("#{}\n{}".format(test_case, *i))
-        else:
-            print(*i)
-        
-              
+    arr=[[0]*(n+1) for _ in range(n+1)] #패딩포함해서 0으로 초기화
+    arr[1][1]=1 #초기값
+    for i in range(2, n+1): #i=1은 윗줄에서 이미 초기화를 함
+        for j in range(1,i+1):
+            arr[i][j] = arr[i-1][j-1]+arr[i-1][j]
+    print(f"#{test_case}")
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(arr[i][j], end=" ")
+        print()
+
     # ///////////////////////////////////////////////////////////////////////////////////
