e45c24d3d1c2496fca3eedae16ca5ece90b026c3
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-10-12 03:20:54 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,8 +1,7 @@
 n, m = map(int,input().split())
 a = [list(map(int,input().split())) for _ in range(n)]
 b = [list(map(int,input().split())) for _ in range(n)]
-result = [[0]*m for _ in range(n)]
 for i in range(n) :
     for j in range(m):
-        result[i][j] = a[i][j]+b[i][j]
-    print(' '.join(map(str,result[i])))
+        print(a[i][j] + b[i][j], end=" ")
+    print()
