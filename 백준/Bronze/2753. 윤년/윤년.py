fe93fc69dbeb0a53ecbed76f5bf5cecfc050be92
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-10-12 12:17:23 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,7 +1,7 @@
-xyear = int(input())
-
-if ((xyear%4==0 and xyear%100!=0)or(xyear%400==0)):
-    print("1")
+year = int(input())
+if year%400 ==0:
+    print(1)
+elif year%4==0 and year%100!=0:
+    print(1)
 else:
-  print("0")
-        
+    print(0)
