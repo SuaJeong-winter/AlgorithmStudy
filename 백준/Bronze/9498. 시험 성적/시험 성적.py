71324b844c2c2804f277939a86e00132a304a860
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-01 15:21:32 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,13 @@
-score=int(input())
-if (90<=score<=100):
+import sys
+
+score=int(sys.stdin.readline().rstrip())
+if score>=90:
     print("A")
-elif (score>=80):
+elif score>=80:
     print("B")
-elif (score>=70):
+elif score>=70:
     print("C")
-elif (score>=60):
+elif score>=60:
     print("D")
 else:
     print("F")
