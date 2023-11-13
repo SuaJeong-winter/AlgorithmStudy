cb2a7b7f5167f75b3ff9b21bb62aeb2034ac0af5
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-14 03:40:07 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 91 ms, Memory: 43,768 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -1,15 +1,9 @@
-T = int(input()) #테스트케이스
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    p, q, r, s, w = map(int, input().split())  
-    coma=w*p 
-    comb=0
-    if w<r:
-        comb=q
+T=int(input())
+for t in range(1,T+1):
+    p,q,r,s,w=map(int,input().split())
+    a_price=p*w
+    if w<=r:
+        b_price=q
     else:
-        comb=q+s*(w-r)
-    
-    result=min(coma, comb)
-    print(f"#{test_case} {result}")
-    # ///////////////////////////////////////////////////////////////////////////////////
+        b_price=q+(w-r)*s
+    print(f"#{t} {min(a_price,b_price)}")
