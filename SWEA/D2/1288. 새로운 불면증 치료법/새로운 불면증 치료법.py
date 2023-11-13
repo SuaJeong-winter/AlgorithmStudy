99ca9343afcdd21083ce933216e199162fffad0d
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-14 04:34:10 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 128 ms, Memory: 54,848 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,19 +1,14 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    num= int(input())  #초기값
-    start=1 #num*start로 배수값을 만들 수 
-    sheep = [0]*10 #1부터 0 까지 모든 수를 보는지 카운팅하는 1차 배열
-    
-    while min(sheep)<=0:  #리스트의 모든 값이 1이상일 때 while문 탈출
-        snum=str(num*start)  #num을 문자열로 바꿔준다, 배수값은 여기서 만들어짐
-        for i in range(len(snum)):
-            sheep[int(snum[i])]+=1
-        start+=1
-    print(f"#{test_case} {snum}")
-    
-    
-    # ///////////////////////////////////////////////////////////////////////////////////
+T=int(input())
+for test_case in range(1,T+1):
+    n=int(input())
+    num_list=[0]*10 #0~9까지의 수를 카운트할 리스트
 
-          
+    k=1
+    while(1):
+        num1= n*k
+        for num in str(num1):
+            num_list[int(num)]+=1
+        k += 1
+        if min(num_list)!=0:
+            print(f"#{test_case} {num1}")
+            break
