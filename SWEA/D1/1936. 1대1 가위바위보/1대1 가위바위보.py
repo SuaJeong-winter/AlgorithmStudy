86fe43ad51216a0aa7e18cfb37c24ece00f97c0f
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-16 09:57:18 +0900

    [D1] Title: 1대1 가위바위보, Time: 94 ms, Memory: 42,724 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -1,8 +1,5 @@
-Anum, Bnum = map(int, input().split())
-if (Anum==2 and Bnum==1) or (Anum==3 and Bnum==2) or (Anum==1 and Bnum==3) :
+a,b=map(int,input().split())
+if (a==2 and b==1) or (a==3 and b==2) or (a==1 and b==3):
     print("A")
-elif (Anum==3 and Bnum==1) or (Anum==1 and Bnum==2) or (Anum==2 and Bnum==3):
-    print("B")
 else:
-    pass
-
+    print("B")
