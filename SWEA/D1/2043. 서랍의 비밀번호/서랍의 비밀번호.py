ca75ad5ea3dba55bcb1a299e535589059cf580f1
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-16 10:15:37 +0900

    [D1] Title: 서랍의 비밀번호, Time: 95 ms, Memory: 42,988 KB -BaekjoonHub

--- SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.py
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.py
@@ -1,11 +1,2 @@
-#비밀번호 P, 초기값 K 입력받기 
-p, k = map(int, input().split())
-cnt=1
-while k != p:
-    cnt+=1
-    k+=1
-    if p == k:
-        break
-print(cnt)
-
-    
+p,k = map(int, input().split())
+print(p-k+1)
