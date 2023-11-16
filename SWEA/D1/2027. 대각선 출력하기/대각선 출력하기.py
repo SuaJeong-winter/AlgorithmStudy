22863117f0a780471c70fb547e13a83b4a54c3ee
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-16 10:10:00 +0900

    [D1] Title: 대각선 출력하기, Time: 94 ms, Memory: 42,724 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
@@ -1,15 +1,7 @@
-#나중에 꼭 다시 풀어볼 것
-
-pic = [[0 for i in range(5)] for j in range(5)]
 for i in range(5):
-  for j in range(5):
-    if i==j:
-      pic[i][j]="#"
-    else:
-      pic[i][j]="+"
-
-for i in pic:
-  for j in i:
-    print(j,end="")
-  print()
-
+    for j in range(5):
+        if i==j:
+            print("#",end="")
+        else:
+            print("+",end="")
+    print()
