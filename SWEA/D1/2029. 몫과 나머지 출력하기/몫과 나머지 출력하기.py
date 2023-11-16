d4d121f4a63a03e44cb21bda27e9c9c99c42f668
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2023-11-16 10:12:22 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 98 ms, Memory: 41,944 KB -BaekjoonHub

--- SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.py
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.py
@@ -1,9 +1,4 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    a, b = map(int, input().split())
-    result1= a//b
-    result2 = a%b
-    print(f"#{test_case} {result1} {result2}")
-    # ///////////////////////////////////////////////////////////////////////////////////
+T =int(input())
+for test_case in range(1,T+1):
+    a,b=map(int,input().split())
+    print(f"#{test_case} {a//b} {a%b}")
