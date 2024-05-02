e67e6085607b00b0f555f81305a3e76c36051934
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-05-03 08:13:46 +0900

    [Bronze IV] Title: 줄번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4470. 줄번호/줄번호.py
+++ 백준/Bronze/4470. 줄번호/줄번호.py
@@ -1,6 +1,12 @@
 import sys
 
-num= int(sys.stdin.readline())
-for i in range(1,num+1):
-    sentence=sys.stdin.readline().rstrip()
-    print(f"{i}. {sentence}")
+def solution(numbers):
+    for i in range(1,numbers+1):
+        titles = sys.stdin.readline().rstrip()
+        print(f'{i}. {titles}')
+
+numbers = int(sys.stdin.readline())
+solution(numbers)
+
+
+    
