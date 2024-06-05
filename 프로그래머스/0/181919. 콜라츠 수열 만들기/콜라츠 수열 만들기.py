5e99efa4549d3cd7f1f10906b62eddb0d0389083
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-06-05 18:56:41 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    while (n!=1):
+        answer.append(n)
+        if n%2==0:
+            n=n//2
+        elif n%2==1:
+            n=3*n+1
+    answer.append(1)
+    return answer
