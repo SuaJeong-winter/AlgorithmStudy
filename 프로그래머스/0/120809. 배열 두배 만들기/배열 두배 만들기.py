eae726864c989319d9f0330e5b4e1b6458f8376a
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-23 02:14:53 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,6 +1,5 @@
 def solution(numbers):
     answer = []
-    for i in range(len(numbers)):
-        answer.append(numbers[i]*2)
-
+    for num in numbers:
+        answer.append(num*2)
     return answer
