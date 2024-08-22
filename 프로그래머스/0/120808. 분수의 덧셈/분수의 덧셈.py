786dcedb99467ed9689c2af41ee3c7429a9fbd5f
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-23 02:09:36 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,13 +1,10 @@
+import math
+
 def solution(numer1, denom1, numer2, denom2):
     answer = []
     numer = numer1*denom2+numer2*denom1
     denom = denom1*denom2
-    for i in range(denom,0,-1):
-        if numer%i==0 and denom%i==0:
-            numer=numer//i
-            denom=denom//i
-        else:
-            pass
-    answer.append(numer)
-    answer.append(denom)
+
+    answer.append(numer//math.gcd(numer,denom))
+    answer.append(denom//math.gcd(numer,denom))
     return answer
