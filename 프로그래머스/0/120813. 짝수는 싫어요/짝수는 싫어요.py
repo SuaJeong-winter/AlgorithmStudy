c512df8ea9f80e6c22d670617a3a64823c0184c3
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-30 15:22:53 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,9 +1,6 @@
 def solution(n):
     answer = []
-    for i in range(1,n+1):
-        if i%2!=0:
-            answer.append(i)
-        else:
-            pass
-            
+    for num in range(n+1):
+        if num%2 ==1:
+            answer.append(num)
     return answer
