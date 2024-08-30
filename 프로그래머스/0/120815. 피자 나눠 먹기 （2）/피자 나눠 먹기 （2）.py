a4bda0221bf2588169131023efa2af2636c6fe58
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-30 15:45:09 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,9 +1,7 @@
-def solution(people):
-    pizza = 1
-    while (1):
-        if (6*pizza)%people ==0:
-            return pizza
+def solution(n):
+    answer = 1
+    while(1):
+        if (6*answer)%n==0:
+            return answer
         else:
-            pizza+=1
-            
-
+            answer+=1
