21e41b27478b2c3acc3e1b888e0613d9253c0bb9
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-31 23:11:38 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,5 +1,5 @@
 def solution(my_string, n):
     answer = ''
-    for i in range (len(my_string)):
-        answer+=my_string[i]*n
+    for alpha in my_string:
+        answer+=alpha*n
     return answer
