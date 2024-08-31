b8211c56d5c1c9fa85498713423b4fff87d5d1bf
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-31 22:59:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,5 +1,5 @@
 def solution(my_string):
     answer = ''
-    for alpha in range(len(my_string)-1,-1,-1):
-        answer+=my_string[alpha]
+    for i in range(len(my_string)-1,-1,-1):
+        answer+=my_string[i]
     return answer
