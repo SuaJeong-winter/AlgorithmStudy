c23ffd5d456be2c42827a84c0af2914bf50cc48b
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-08-31 23:06:30 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,11 +1,8 @@
 def solution(num_list):
     answer = [0,0]
-    cnt_odd, cnt_even = 0,0
-    for index in num_list:
-        if index%2 ==0:
-            cnt_even+=1
+    for num in num_list:
+        if num%2==0:
+            answer[0]+=1
         else:
-            cnt_odd+=1
-    answer[0],answer[1] = cnt_even,cnt_odd
-
+            answer[1]+=1
     return answer
