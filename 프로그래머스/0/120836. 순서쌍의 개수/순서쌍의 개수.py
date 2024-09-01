b80e17126f9e1070a70d895f516c550c9d9ec4ac
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-09-01 11:53:46 +0900

    [level 0] Title: 순서쌍의 개수, Time: 86.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,6 @@
 def solution(n):
-    num_list = []
-    for i in range(1,n+1):
-        if n%i==0:
-            num_list.append(i)
-    return len(num_list)
+    answer = 0
+    for num in range(1,n+1):
+        if n%num==0:
+            answer+=1
+    return answer
