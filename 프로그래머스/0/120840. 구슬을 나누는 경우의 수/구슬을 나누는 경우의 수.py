2632b4e67e012d4fd801b8272d0d3aa074cf64f1
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2024-09-01 17:30:28 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,4 +1,11 @@
-from math import comb
+import math
 
 def solution(balls, share):
-    return comb(balls,share)
+    total, dup=1,1
+    for upper in range(balls,balls-share,-1):
+        total*=upper
+    for lower in range(1,share+1):
+        dup*=lower
+    result1 = total//math.gcd(total,dup)
+    result2 = dup//math.gcd(total,dup)
+    return result1//result2
