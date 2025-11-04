9eed49eb3e6c0747cc5d5278dd5fa2d94dab7f65
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2025-11-05 02:54:24 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,7 +1,4 @@
 def solution(array):
-    # 배열을 정렬
-    arr=sorted(array)
-    # 배열의 중앙값 찾기
-    answer=arr[len(array)//2]
-
+    sorted_arr = sorted(array)
+    answer = sorted_arr[len(array)//2]
     return answer
