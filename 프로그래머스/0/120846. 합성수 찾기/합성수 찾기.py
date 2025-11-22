d9bee48e420aeffc59ffd5c84fe9b444d2c3d22d
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2025-11-23 02:22:36 +0900

    [level 0] Title: 합성수 찾기, Time: 0.42 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,12 +1,3 @@
-import math
-
 def solution(n):
     answer = 0
-    for num in range(1,n+1):
-        cnt=0
-        for val in range(1,num+1):
-            if num%val==0:
-                cnt+=1
-        if cnt>2:
-            answer+=1
     return answer
