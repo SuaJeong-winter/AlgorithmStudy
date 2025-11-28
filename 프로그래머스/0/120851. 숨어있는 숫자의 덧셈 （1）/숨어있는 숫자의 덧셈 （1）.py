a03a0c78682cd1586550ad7816d5cfe33caddf85
Jeong Sua <131633378+SuaJeong-winter@users.noreply.github.com>
2025-11-29 01:32:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,7 +1,8 @@
 def solution(my_string):
     answer = 0
-    num_list = ['0','1','2','3','4','5','6','7','8','9']
+
     for num in my_string:
-        if num in num_list:
+        if num.isdigit():
             answer+=int(num)
+
     return answer
